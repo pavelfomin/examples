be3e1ed1f7aba1172e145406b51a09e554a111ec
Pavel Fomin <p.v.fomin@gmail.com>
2016-11-24 11:21:02 -0600

    Added sed expression and an example.

--- scripts/diff-content.sh
+++ scripts/diff-content.sh
@@ -2,16 +2,17 @@
 # Returns a diff status of the comparison between the current content of the http response and a previously saved one. 
 ##
 
-set -x #echo commands
+#set -x #echo commands
 set -e #exit on error
 
 if [ -z "$1" ]
 then
-	echo "Usage: $0 <url>"
+	echo "Usage: $0 <url> [<sed expression>]"
 	exit 1
 fi
 
 url=$1
+sedExpression=$2
 
 contentBaseDir=${TMP}
 contentFileName=content.html
@@ -27,6 +28,13 @@ diffFile=${historyDir}/${contentFileName}.${dateTime}.diff
 mkdir -p ${historyDir}
 
 curl -v --fail "${url}" 2>${logFile} 1>${currentContent} 
+
+if [ ${sedExpression} ]
+then
+	mv ${currentContent} ${currentContent}.raw
+	sed -e "${sedExpression}" ${currentContent}.raw > ${currentContent}
+	rm  ${currentContent}.raw
+fi
 
 if [ -e "${previousContent}" ]
 then
---
+++ scripts/diff-example.sh
@@ -0,0 +1,2 @@
+$(dirname "$0")/diff-content.sh http://seattle.mid.ru/vyezdnye-sessii 's/authToken="[^"]*"//g'
+#echo status=$?
